872d910795fdd2e1487a77dbd0aa1af51adb7ecd
nicoleta.fratila <nicoleta.fratila@softvision.com>
2022-10-28 14:08:56 +0300

    Small design changes

--- RecipeBook.Shared/Models/EditRecipeModel.cs
+++ RecipeBook.Shared/Models/EditRecipeModel.cs
@@ -11,6 +11,7 @@ namespace RecipeBook.Shared.Models
         [StringLength(100)]
         public string Name { get; set; }
 
+        [MaxLength(512000)]
         public byte[]? ImageContent { get; set; }
 
         public string? ImageUrl { get; set; }
