7bf9a6fcf65915af14ef8d7c6ee24f2472d4fa33
nicoleta.fratila <nicoleta.fratila@softvision.com>
2023-06-27 10:11:01 +0300

    Added images for ingredients

--- Common.Data.Entities/Ingredient.cs
+++ Common.Data.Entities/Ingredient.cs
@@ -5,6 +5,7 @@ namespace Common.Data.Entities
     public class Ingredient : Entity<int>
     {
         public string? Name { get; set; }
+        public byte[]? ImageContent { get; set; }
 
         [ForeignKey("UnitId")]
         public Unit? Unit { get; private set; }
