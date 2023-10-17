31137e7ed931aba06d35e752509021404ffd93b4
nicoleta.fratila <nicoleta.fratila@softvision.com>
2023-10-17 11:55:00 +0300

    Added mediatr

---
+++ RecipeBook.Api/Features/Product/Queries/GetProductCategory/GetProductCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using RecipeBook.Shared.Models;
+
+namespace RecipeBook.Api.Features.Product.Queries.GetProductCategory
+{
+    public class GetProductCategoryQuery : IRequest<IList<ProductCategoryModel>>
+    {
+    }
+}
---
+++ RecipeBook.Api/Features/Product/Queries/GetProductCategory/GetProductCategoryQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using RecipeBook.Api.Features.Product.Queries.GetProductCategory;
+using RecipeBook.Api.Repositories;
+using RecipeBook.Shared.Models;
+
+namespace RecipeBook.Api.Features.Product.Queries.GetProducts
+{
+    public class GetProductCategoryQueryHandler : IRequestHandler<GetProductCategoryQuery, IList<ProductCategoryModel>>
+    {
+        private readonly IProductCategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetProductCategoryQueryHandler(IProductCategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<ProductCategoryModel>> Handle(GetProductCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var results = await _repository.GetAllAsync();
+            return _mapper.Map<IList<ProductCategoryModel>>(results).OrderBy(r => r.DisplaySequence).ToList();
+        }
+    }
+}
