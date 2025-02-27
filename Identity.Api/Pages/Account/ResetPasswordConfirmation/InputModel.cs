a23143d541ff7e9855455380fdf9d2a7b97c26f7
nfratila <nfratila@suvoda.com>
2025-02-27 17:28:31 +0200

    Added views for identity

---
+++ Identity.Api/Pages/Account/ResetPasswordConfirmation/InputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Api.Pages.Account.ResetPasswordConfirmation
+{
+    public class InputModel
+    {
+        public string? Email { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
---
+++ Identity.Api/Program.cs
@@ -0,0 +1,21 @@
+﻿using Identity.Api;
+using Serilog;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        host.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .UseSerilog();
+    }
+}
